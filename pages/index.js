9ecae694b9dfa59658575e20f612d4e4491f4707
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-11-14 22:04:08 -0800

    update: removeNavbar component from index.js          feature: none

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import About from '../components/About'
-import Contact from '../components/Contact'
-import  Main  from '../components/Main'
-import Navbar from '../components/Navbar'
+import Contact from '../components/ContactUs'
+import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
@@ -15,14 +13,11 @@ export default function Home() {
         <title>Tyler | Software Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Navbar />
     <Main />
     <About />
     <Skills />
     <Projects />
-    <Contact/>
-    
-      
+    <Contact/> 
     </div>
   )
 }
