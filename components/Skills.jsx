2ba6f80f593aee54fd5acbce4eb0afdc416b4431
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-11-16 19:48:25 -0800

    update.

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -26,6 +26,38 @@ const Skills = () => {
               </div>
             </div>
           </div>
+          {/* AWS */}
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <Image
+                  src='/../public/assets/skills/aws.png' 
+                  width='64' 
+                  height='64' 
+                  alt='/' 
+                />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>AWS</h3>
+              </div>
+            </div>
+          </div>
+          {/* Docker */}
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <Image
+                  src='/../public/assets/skills/docker.png' 
+                  width='64' 
+                  height='64' 
+                  alt='/' 
+                />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Docker</h3>
+              </div>
+            </div>
+          </div>
           {/* CSS */}
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
